7603563c0034e85569e8dcc6eac86437d81d0ac3
Aswin Sujith Varghese <73499655+aswinsujithvarghese365@users.noreply.github.com>
2022-09-03 14:34:16 +0530

    Add files via upload

---
+++ Contact_us.py
@@ -0,0 +1,15 @@
+# Contact Us
+
+def contact_DHR():
+    print("Contact Us")
+    print("==========")
+    print("____________________________________________________________","\n")
+    print("For Technical Support","Contact: +91 799 8877666","Email: helpdesk@dhr.in",sep="\n")
+    print("____________________________________________________________","\n")
+    print("Office of Secretary DHR :","Department of Health Research","2nd Floor,IRCS Building,",
+          "1,Red Cross Road, New Delhi - 110001",sep="\n")
+    print("------------------------------------------------------------","\n")
+    print("Web Information Managers :",
+          "Shri Aswin Sujith Varghese,Shri Adarsh A,Shri Vignesh,",
+          "Under Secretary, Department of Health Research",
+          "Email : depthealthresearch@dhr.in","Office No. :+91 755 9988111",sep="\n")
